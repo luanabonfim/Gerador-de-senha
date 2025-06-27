6e17f265ca34e945cfff974fdcfe6cde33c377ad
luanabonfim <141340853+luanabonfim@users.noreply.github.com>
2025-06-27 08:26:15 -0300

    .

--- main.js
+++ main.js
@@ -68,10 +68,11 @@ function classificaSenha(){
     if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 }
-else {
+else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
 forcaSenha.classList.add('media');
-}
+else if (tamanhoSenha <= 5){
+
 
     forcaSenha.classList.add('forte');
-
+}
 }
